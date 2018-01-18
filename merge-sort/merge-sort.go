97f9d648e2cd99325aaf8837fb7f428095267b3e
rahul10992 <rahul10992@gmail.com>
2018-01-17 20:51:31 -0800

    finallyn got merge sort working..

--- merge-sort/merge-sort.go
+++ merge-sort/merge-sort.go
@@ -3,14 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	//sampleArray := [4]int{4, 3, 2, 1}
+	//sampleArray := [5]int{5, 4, 3, 2, 1}
 	sampleArray := [10]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
-	mergeSort(0, 9, sampleArray[:])
+	//sampleArray := [2]int{2, 1}
+	mergeSort(0, len(sampleArray)-1, sampleArray[:])
 	fmt.Println(sampleArray)
 }
 
 func mergeSort(p int, r int, array []int) {
 	fmt.Println("p = ", p, "r=", r, "array = ", array)
-	if r >= p {
+	if p < r {
 		q := (p + r) / 2
 		mergeSort(p, q, array)
 		mergeSort(q+1, r, array)
@@ -21,36 +24,38 @@ func mergeSort(p int, r int, array []int) {
 func merge(p int, q int, r int, array []int) {
 	fmt.Println("p = ", p, "q=", q, "r=", r)
 	var n1, n2, leftIndex, rightIndex int
-	n1 = (p - q) / 2
-	n2 = (q + 1 - r) / 2
-	leftIndex = p
-	rightIndex = q + 1
+	n1 = q - p + 1
+	n2 = r - q
+	leftIndex = 0
+	rightIndex = 0
 	L := make([]int, n1)
 	R := make([]int, n2)
-	for i := p; i <= q; i++ {
-		L[i] = array[i]
+	for i := 0; i < n1; i++ {
+		L[i] = array[p+i]
 	}
-	for i := q + 1; q <= r; i++ {
-		R[i] = array[i]
+	for i := 0; i < n2; i++ {
+		R[i] = array[q+1+i]
 	}
 	i := p
-	for leftIndex <= q && rightIndex <= r {
-		if L[i] <= R[i] {
-			array[i] = L[i]
+	for leftIndex < n1 && rightIndex < n2 {
+		if L[leftIndex] <= R[rightIndex] {
+			array[i] = L[leftIndex]
 			leftIndex++
 		} else {
-			array[i] = R[i]
+			array[i] = R[rightIndex]
 			rightIndex++
 		}
 		i++
 	}
-	for leftIndex <= q {
-		array[leftIndex] = L[leftIndex]
+	for leftIndex < n1 {
+		array[i] = L[leftIndex]
 		leftIndex++
+		i++
 	}
-	for rightIndex <= r {
-		array[rightIndex] = R[rightIndex]
+	for rightIndex < n2 {
+		array[i] = R[rightIndex]
 		rightIndex++
+		i++
 	}
 	fmt.Println("Intermediate Step:", array)
 }
